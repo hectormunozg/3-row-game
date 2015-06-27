aaa4b8756df129f3855b93ea289f3233cd044630
hectormunozg <hectormunozgarcia@gmail.com>
2015-06-27 09:43:08 +0100

    initial js file

---
+++ app.js
@@ -0,0 +1,16 @@
+// object Game
+var game = {
+	playerName   : "Player 1",
+	computerName : "Computer",
+	totalGames   : 0,
+	playerWins   : 0,
+	computerWins : 0,
+	board 		 : [[0, 0, 0], [0, 0, 0], [0, 0, 0]],	
+	gameLang	 : [["inglés"], ["español"]],
+	newGame 	 : function() {},
+	computerMove : function() {},
+	checkPos	 : function() {},
+	resetStats	 : function() {},
+	changeLang	 : function() {}
+};
+
